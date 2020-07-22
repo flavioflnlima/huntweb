f124dfe38d451481b8ea10aca45edc43de5bb702
Flavio Lima do nascimento <flavioflnlima@gmail.com>
2020-07-21 22:01:54 -0300

    feat: iniciando o huntweb

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//criando componente por class
+// class App extends Component{
+//   render() {
+//     return (
+//       <div className="App">
+//         <Header/>
+//       </div>
+//     );
+//   }
+// }
 
+//criando componente por function
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header/>
+//     </div>
+//   );
+// }
+
+//criando o componente por function menos verbosa
+const App = () => (
+  <div className="App">
+    <Header></Header>
+  </div>
+);
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles.css";
+
+//forma verbosa
+// const Header = function () {
+//   return <header id="main-header">JSHunt</header>;
+// };
+
+const Header = () => {
+  return <header id="main-header">JSHunt</header>;
+};
+
+export default Header;
