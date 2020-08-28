ade8e527b4de2588d085598352e0e394d41cc013
Flavio Lima do nascimento <flavioflnlima@gmail.com>
2020-08-27 22:10:16 -0300

    feat: estilizado o list

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import './styles.css'
+
 
 export default class Main extends Component {
 
@@ -18,13 +20,21 @@ export default class Main extends Component {
         this.setState({products : response.data.docs }); 
     }
 
-    render(){
+    render() {
+        
+        const { products } = this.state;
+
         return (
-            <div className="products-list" >
-                {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
-                ))}
-            </div>
-        )
+          <div className="product-list">
+            {products.map((product) => (
+              <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>  
+              </article>
+            ))}
+          </div>
+        );
     }
 }
