82f7c6380093d5824a7df6fb31c7393e47f396ea
Flavio Lima do nascimento <flavioflnlima@gmail.com>
2020-08-27 21:34:53 -0300

    feat: listado os produtos da api

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,7 +3,28 @@ import api from '../../services/api';
 
 
 export default class Main extends Component {
+
+    state = {
+        products: []
+    }
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () =>{
+        const response = await api.get('/products');
+
+        this.setState({products : response.data.docs }); 
+    }
+
     render(){
-        return <h1>Hello Flavio</h1>
+        return (
+            <div className="products-list" >
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        )
     }
 }
